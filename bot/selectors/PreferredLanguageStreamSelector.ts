d85e2792203566a0c6e40183724705c75a751b48
Neil Armitage <flusaka@gmail.com>
2026-02-21 14:08:43 +0200

    feat: Implement PreferredLanguageStreamSelector

---
+++ bot/selectors/PreferredLanguageStreamSelector.ts
@@ -0,0 +1,54 @@
+import { injectable } from "inversify";
+import type { ChannelConfiguration } from "../domain/ChannelConfiguration";
+import type { Stream } from "../domain/data/Stream";
+import type { Language } from "../domain/Language";
+import type { StreamSelector } from "./interfaces/StreamSelector";
+
+@injectable()
+export class PreferredLanguageStreamSelector implements StreamSelector {
+  findPreferredStream(
+    streams: Stream[],
+    channelConfig: ChannelConfiguration,
+  ): Stream | undefined {
+    function getPriorityIndexForStream(
+      stream: Stream,
+      preferredLanguage: Language,
+    ): number {
+      if (
+        stream.official &&
+        stream.main &&
+        stream.language === preferredLanguage
+      ) {
+        return 0;
+      } else if (stream.official && stream.language === preferredLanguage) {
+        return 1;
+      } else if (stream.language === preferredLanguage) {
+        return 2;
+      } else if (stream.official && stream.main) {
+        return 3;
+      } else if (stream.official) {
+        return 4;
+      }
+      return 5;
+    }
+
+    if (streams.length === 0) {
+      return;
+    }
+
+    let preferredStream: Stream | undefined;
+    let currentPriorityIndex = Number.MAX_VALUE;
+    for (const stream of streams) {
+      const priority = getPriorityIndexForStream(
+        stream,
+        channelConfig.preferredLanguage,
+      );
+      if (priority < currentPriorityIndex) {
+        currentPriorityIndex = priority;
+        preferredStream = stream;
+      }
+    }
+
+    return preferredStream;
+  }
+}
